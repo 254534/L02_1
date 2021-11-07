e770b8476066c89bc6bc296098c07e459e71e50b
ksproska <kamila.sproska@gmail.com>
2021-11-07 09:14:21 +0100

    Initial commit

---
+++ app/src/main/java/com/example/l02_1/ActivityLeft.kt
@@ -0,0 +1,11 @@
+package com.example.l02_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityLeft : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_left)
+    }
+}
